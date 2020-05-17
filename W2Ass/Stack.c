4c1e7bfe38a6cbe8cedea9479cdc6a1aab6b3dfb
kmrnicholson <kohdy.nicholson@gmail.com>
2020-05-17 14:38:27 +0200

    WHY

--- W2Ass/Stack.c
+++ W2Ass/Stack.c
@@ -106,7 +106,7 @@ bool pop(int *stack, int *size, int *to_return)
 	bool result = false;
 	int sp = *size;
 
-	if(*size > 1){
+	if(*size > 0){
 		*to_return = stack[sp];
 		sp--;
 		*size = sp;
@@ -136,7 +136,7 @@ bool peek(int *stack, int *size, int *to_return)
 	bool result = false;
 	int sp = *size;
 
-	if(*size > 1){
+	if(*size > 0){
 		*to_return = stack[sp];
 		result = true;
 	}
