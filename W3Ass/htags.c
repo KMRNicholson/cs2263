d22d280d8dd75fcaba7481d8cabaa39c971b2b0d
kmrnicholson <kohdy.nicholson@gmail.com>
2020-05-24 10:17:32 +0200

    Adding files for assignment 3

---
+++ W3Ass/htags.c
@@ -0,0 +1,21 @@
+/* 
+    htags.c
+
+    Description:
+    Program that counts the occurence of html tags from std input.
+    
+    Author: 
+    Kohdy Nicholson
+    
+    Date: 
+    2020-05-24
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char** argv){
+    printf("argc: %d\n", argc);
+    return EXIT_SUCCESS;
+}
