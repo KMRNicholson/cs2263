228f9f02eb538942a2a9e472b193a79992cda9f8
kmrnicholson <kohdy.nicholson@gmail.com>
2020-06-06 19:03:30 +0200

    Fixing more amateur mistakes

--- W5Ass/loadRoutesTest.c
+++ W5Ass/loadRoutesTest.c
@@ -10,8 +10,7 @@ int main(int argc, char** argv){
     FILE* file = fopen(argv[1], "r");
     pLineListFile lineListFile = createLineListFile(file);
     fclose(file);
-    int i = 0;
-    int j = 0;
     lineListFileToString(lineListFile);
     freeLineListFile(lineListFile);
+    return EXIT_SUCCESS;
 }
